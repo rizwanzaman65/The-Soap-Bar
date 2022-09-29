f9851663eeae7a41fe414d488bfb37c9e9a2ea47
Rizwan <rizwan2203e@aptechgdn.net>
2022-09-29 13:19:49 +0500

    The Soap Bar

---
+++ script.js
@@ -0,0 +1,48 @@
+function change1()
+{
+    document.getElementById ("img1").src="images/pic12.jpg"
+
+}
+
+function change2()
+{
+    document.getElementById ("img1").src="images/pic5.webp"
+
+}
+
+function change3()
+{
+    document.getElementById ("img2").src="images/pic13.jpg"
+
+}
+
+function change4()
+{
+    document.getElementById ("img2").src="images/pic6.webp"
+
+}
+
+function change5()
+{
+    document.getElementById ("img3").src="images/pic14.jpg"
+
+}
+
+function change6()
+{
+    document.getElementById ("img3").src="images/pic7.webp"
+
+}
+
+
+function change7()
+{
+    document.getElementById ("img4").src="images/pic15.jpg"
+
+}
+
+function change8()
+{
+    document.getElementById ("img4").src="images/pic8.webp"
+
+}
